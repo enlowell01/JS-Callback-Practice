5ddcded53308f6d1d7b79e69b0ffeb05a4634818
Evan <evanlowell01@gmail.com>
2023-03-12 12:50:12 -0400

    final updates part 4

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-function newImage (pth, lt, bt) {
-    let object = document.createElement('img')
-    object.src = pth
-    object.style.position = 'fixed'
-    object.style.left = lt + 'px'
-    object.style.bottom = bt + 'px'
-    document.body.append(object)
-    return object
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
+    if(direction === 'west'){
+        character.src = `assets/green-character/west.gif`
+    }
+    if(direction === 'north'){
+        character.src = `assets/green-character/north.gif`
+    }
+    if(direction === 'east'){
+        character.src = `assets/green-character/east.gif`
+    }
+    if(direction === 'south'){
+        character.src = `assets/green-character/south.gif`
+    }
 }
 
-newImage('assets/green-character.gif', 100, 100);
-
-newImage('assets/pine-tree.png', 450, 200);
-
-newImage('assets/pillar.png', 350, 100);
-
-newImage('assets/pine-tree.png', 450, 200);
-
-newImage('assets/crate.png', 150, 200);
-
-newImage('assets/well.png', 500, 425);
-
-function newItem(pth, lt, bt){
-    let object = newImage(pth, lt, bt)
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    })
-}
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
-newItem('assets/sword.png', 500, 405);
 
-newItem('assets/shield.png', 165, 185);
-
-newItem('assets/staff.png', 600, 100);
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
